525dc109090edce76235c1d73aa1b96f275d7a0b
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-06-04 13:44:48 -0400

    updated finder function

--- sftp_loader.py
+++ sftp_loader.py
@@ -3,7 +3,7 @@ import paramiko
 
 host = config('SFTP_HOST')  # Init transport
 port = 22
-transport = paramiko.Transport((host, port))
+transport = paramiko.Transport(host, port)
 
 user = config('SFTP_CLIENT')  # Transport Authentication
 pwd = config('SFTP_SECRET')
@@ -11,17 +11,17 @@ transport.connect(username=user, password=pwd)
 
 sftp = paramiko.SFTPClient.from_transport(transport)
 
-ex_ls = sftp.listdir(path='./Import')
-print(len(ex_ls))
+ex_imports = sftp.listdir(path='./Import')
+print(len(ex_imports))
 
 
-def finder(sub_str):
-    return [file for file in ex_ls if sub_str in file]
+def finder(sub_str, path):
+    return [file for file in path if sub_str in file]
 
 
-e_dupes = finder('Email_Duplicate_Suppression')
-brokers = finder('Brokers_to_Email')
-locations = finder('Supreme_Locations')
+e_dupes = finder('Email_Duplicate_Suppression', ex_imports)
+brokers = finder('Brokers_to_Email', ex_imports)
+locations = finder('Supreme_Locations', ex_imports)
 print(e_dupes)
 
 sftp.close()
